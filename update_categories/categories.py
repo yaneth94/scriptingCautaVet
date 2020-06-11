9f319ee01be2208eca9e0cbf68acedc66e8a2cce
Zoila <zoilavillatoro6694@outlook.com>
2020-06-11 00:56:46 -0600

    updated autopep8

--- update_categories/categories.py
+++ update_categories/categories.py
@@ -1,4 +1,4 @@
-from settings import  api_key_master, api_token_master, api_key, api_token
+from settings import api_key_master, api_token_master, api_key, api_token
 import requests
 import json
 
@@ -9,7 +9,7 @@ headers_master = {
     'x-vtex-api-appkey': api_key_master,
     'x-vtex-api-apptoken': api_token_master
 }
-#header vetrob2c
+# header vetrob2c
 headers = {
     'content-type': "application/json",
     'accept': "application/vnd.vtex.ds.v10+json",
@@ -20,10 +20,12 @@ headers = {
 url_categories = "https://{}.vtexcommercestable.com.br/api/catalog_system/pub/category/tree/20"
 url_create_category = "https://{}.vtexcommercestable.com.br/api/catalog/pvt/category"
 
-response = requests.request("GET", url_categories.format("vetro"), headers=headers_master)
+response = requests.request(
+    "GET", url_categories.format("vetro"), headers=headers_master)
 categories = response.json()
 
-def creation_payload(category, category_id = None):
+
+def creation_payload(category, category_id=None):
     payload = {}
     payload['Name'] = category["name"]
     payload['Keywords'] = ""
@@ -37,58 +39,61 @@ def creation_payload(category, category_id = None):
     payload['IsActive'] = True
     payload['ActiveStoreFrontLink'] = True
     payload['ShowBrandFilter'] = True
-    payload['Score']= None
-    payload['StockKeepingUnitSelectionMode']= "SPECIFICATION"
+    payload['Score'] = None
+    payload['StockKeepingUnitSelectionMode'] = "SPECIFICATION"
     return payload
 
-def children_function(category,father,category_id):
-    print("\033[94m  begin hijo: {} his father is: {} with id of father: {}  \033[0m".format(category["name"],father['name'],father['id']))
 
-    #creation payload
-    payload = creation_payload(category,category_id)
-    #save category
+def children_function(category, father, category_id):
+    print("\033[94m  begin hijo: {} his father is: {} with id of father: {}  \033[0m".format(
+        category["name"], father['name'], father['id']))
+
+    # creation payload
+    payload = creation_payload(category, category_id)
+    # save category
     payload = json.dumps(payload)
-    #print(payload)
-    response = requests.request('POST', url_create_category.format("vetrob2c"), data=payload, headers=headers)
+    # print(payload)
+    response = requests.request('POST', url_create_category.format(
+        "vetrob2c"), data=payload, headers=headers)
     print(response.text)
     response = response.json()
-    #save category_id
+    # save category_id
     category_id = response['Id']
-   
-    #end category
+
+    # end category
 
     # map childrens
     if category['hasChildren']:
         childrens = category["children"]
-        for children in range(0,len(childrens)):
-            children_function(childrens[children],category,category_id)
+        for children in range(0, len(childrens)):
+            children_function(childrens[children], category, category_id)
 
     # end map childrens
-    print("\x1b[1;33m  end hijo: {} his father is: {} with id of father: {}  \033[0m".format(category["name"],father['name'],father['id']))
+    print("\x1b[1;33m  end hijo: {} his father is: {} with id of father: {}  \033[0m".format(
+        category["name"], father['name'], father['id']))
+
 
 for category in categories:
     print("\033[92m  ***************** begin padre: {}  ***************** \033[0m".format(category["name"]))
-    
-    #creation payload
+
+    # creation payload
     payload = creation_payload(category)
-    #save category
+    # save category
     payload = json.dumps(payload)
-    response = requests.request('POST', url_create_category.format("vetrob2c"), data=payload, headers=headers)
+    response = requests.request('POST', url_create_category.format(
+        "vetrob2c"), data=payload, headers=headers)
     print(response.text)
     response = response.json()
-    #save category_id
+    # save category_id
     category_id = response['Id']
-    
-    #end save category
+
+    # end save category
 
     # map childrens
     if category['hasChildren']:
         childrens = category["children"]
-        for children in range(0,len(childrens)):
-            children_function(childrens[children],category, category_id)
-    #end map childrens
-        
+        for children in range(0, len(childrens)):
+            children_function(childrens[children], category, category_id)
+    # end map childrens
+
     print("\033[92m ***************** end padre: {} *********************** \033[0m".format(category["name"]))
-
-
-
--- update_categories/settings.py
+++ update_categories/settings.py
@@ -7,8 +7,8 @@ load_dotenv(dotenv_path=env_path)
 
 
 # there are required to access to api
-api_key_master= os.getenv('API_KEY_MASTER')
-api_token_master= os.getenv('API_TOKEN_MASTER')
+api_key_master = os.getenv('API_KEY_MASTER')
+api_token_master = os.getenv('API_TOKEN_MASTER')
 
-api_key= os.getenv('API_KEY')
-api_token= os.getenv('API_TOKEN')
+api_key = os.getenv('API_KEY')
+api_token = os.getenv('API_TOKEN')
