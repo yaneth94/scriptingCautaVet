649813065b0a4f7e0218e60fd3b4a051b2f1685d
Zoila <zoilavillatoro6694@outlook.com>
2020-06-19 16:28:16 -0600

    finally products tax

--- products_tax/products_tax.py
+++ products_tax/products_tax.py
@@ -1,4 +1,6 @@
 from settings import api_key, api_token, api_key_nexus
+from pathlib import Path
+import os
 import requests
 import json
 
@@ -23,7 +25,7 @@ response = requests.request(
 products_nexus = response.json()
 products_nexus = products_nexus['result']
 
-list_id_produs, list_product, list_tva, list_id_produs_nva = [], [], [], []
+list_id_produs, list_product, list_id_produs_nva = [], [], []
 for product_api in products_nexus:
     id_produs = product_api['id_produs']
     produs = {
@@ -32,16 +34,16 @@ for product_api in products_nexus:
         "cota_tva_ies": product_api['cota_tva_ies']
     }
 
-    list_tva.append(product_api['cota_tva_ies'])
     list_id_produs.append(id_produs)
+    # esto ver despues
     list_id_produs_nva.append(id_produs)
     list_product.append(produs)
 
-list_tva = list(set(list_tva))
+
 list_id_produs = list(set(list_id_produs))
 list_id_produs = str(list_id_produs)
 list_id_produs = list_id_produs.replace("'", '"')
-# print(list_id_produs)
+
 
 # header vtex
 headers = {
@@ -51,6 +53,9 @@ headers = {
     'x-vtex-api-apptoken': api_token
 }
 
+
+print("\033[92m   ****** Begin Query api nexus from products  ****** \033[0m")
+
 # url vtex
 url_vtex_sku = "https://vetro.vtexcommercestable.com.br/api/catalog_system/pub/sku/stockkeepingunitidsbyrefids"
 
@@ -59,28 +64,75 @@ response = requests.request(
     "POST", url_vtex_sku, data=list_id_produs, headers=headers)
 products_vtex = response.json()
 
-filter_vtex_dict = {}
+print("\x1b[1;33m  ****** Ended Query api nexus from products  ****** \033[0m")
 
+
+print("\033[92m   ****** Begin Delete products wth sku none ****** \033[0m")
+filter_vtex_dict = {}
+# eliminando de skus con none
 for product in products_vtex.keys():
     if products_vtex[product]:
         filter_vtex_dict[product] = products_vtex[product]
 
-# print(filter_vtex_dict)
+print("\x1b[1;33m  ****** Ended Delete products wth sku none ****** \033[0m")
+
+
+print("\033[92m   ****** Begin Products equals in vtex ****** \033[0m")
+
+# llamada a api de vtex para sacar los nombres de los productos y que no sean de nexus
+url_sku_id = "https://vetro.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/{}"
+
+# sacando los que coincidan con nexus
 products_nexus_vtex = []
-count_si, count_no = 0, 0
-# print(list(filter_vtex_dict.keys()))
 for product in list_product:
     if product['id_produs'] in list(filter_vtex_dict.keys()):
+        response = requests.request(
+            "GET", url_sku_id.format(filter_vtex_dict[product['id_produs']]), headers=headers)
+        sku_api = response.json()
         produs = {
             "id_produs": product['id_produs'],
-            "den_produs": product['den_produs'],
-            "cota_tva_ies": product['cota_tva_ies']
+            "den_produs": sku_api['SkuName'],
+            "cota_tva_ies": product['cota_tva_ies'],
+            "sku_id": filter_vtex_dict[product['id_produs']]
         }
         products_nexus_vtex.append(produs)
-        count_si = count_si + 1
-    else:
-        count_no = count_no + 1
 
-print(products_nexus_vtex)
-print(count_si)
-print(count_no)
+
+print("\x1b[1;33m  ****** Ended Products equals in vtex ****** \033[0m")
+
+"""
+for product in products_nexus_vtex:
+    response = requests.request(
+        "GET", url_sku_id.format(product['sku_id']), headers=headers)
+    sku_api = response.json()
+    product['den_produs'] = sku_api['SkuName']
+"""
+
+print("\033[92m   ****** Begin Classification by tva ****** \033[0m")
+
+products_tva_dict = {}
+for product in products_nexus_vtex:
+    # asignacion
+    cota_tva = product['cota_tva_ies']
+    name = product['den_produs']
+    if not cota_tva in products_tva_dict.keys():
+        products_tva_dict[cota_tva] = []
+        products_tva_dict[cota_tva].append(name)
+    else:
+        products_tva_dict[cota_tva].append(name)
+
+print("\x1b[1;33m  ****** Ended Classification by tva ****** \033[0m")
+
+print("\033[92m   ****** Begin Create txt file ****** \033[0m")
+
+path_products = Path(__file__).parent.absolute() / 'products'
+for tva in products_tva_dict.keys():
+
+    path_product = path_products / 'tax-{}.txt'.format(tva)
+    file = open(path_product, "wt")
+    for line in products_tva_dict[tva]:
+        file.write(line + '\n')
+        # print(line)
+    file.close()
+
+print("\x1b[1;33m  ****** Ended Create txt file ****** \033[0m")
